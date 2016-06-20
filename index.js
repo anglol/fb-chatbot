e0178931da58fc67a12d16b8e17ef256efa3498f
Richard Lavigne <richard.lavigne87@gmail.com>
2016-06-20 15:30:29 +0200

    try structured message

--- index.js
+++ index.js
@@ -16,37 +16,21 @@ bot.on('error', function(err){
 
 bot.on('message', function(payload, reply){
 
-  var text = payload.message.text,
-      senderId = payload.sender.id;
-
-  console.log(text);
+  var text = payload.message.text;
   
-  api.call(senderId, text, function(text){
+  api.call(payload.sender.id, text, function(text){
     
-    bot.getProfile(senderId, function(err, profile){
-      if (err){
-        console.log('--- error ---');
-        console.log(err);
-        console.log('--- error ---');
-      }
-      
-      if( text.length < 1){
-        text = apologies.getRandom();
-      }
+    bot.getProfile(payload.sender.id, function(err, profile){
+      if (err) console.log(err);
+      if( text.length < 1) text = apologies.getRandom();
         
-      reply({ text }, function(err){
-        if (err){
-          console.log('--- error ---');
-          console.log(err);
-          console.log('--- error ---');
-        }
-
+      reply({"attachment":{"type":"template","payload":{"template_type":"generic","elements":[{"title":"Classic White T-Shirt","image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png","subtitle":"Soft white cotton t-shirt is back in style","buttons":[{"type":"web_url","url":"https://petersapparel.parseapp.com/view_item?item_id=100","title":"View Item"},{"type":"web_url","url":"https://petersapparel.parseapp.com/buy_item?item_id=100","title":"Buy Item"},{"type":"postback","title":"Bookmark Item","payload":"USER_DEFINED_PAYLOAD_FOR_ITEM100"}]},{"title":"Classic Grey T-Shirt","image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png","subtitle":"Soft gray cotton t-shirt is back in style","buttons":[{"type":"web_url","url":"https://petersapparel.parseapp.com/view_item?item_id=101","title":"View Item"},{"type":"web_url","url":"https://petersapparel.parseapp.com/buy_item?item_id=101","title":"Buy Item"},{"type":"postback","title":"Bookmark Item","payload":"USER_DEFINED_PAYLOAD_FOR_ITEM101"}]}]}}}, function(err){
+        if (err) console.log(err);
         console.log(`Echoed back to ${profile.first_name} ${profile.last_name}: ${text}`)
       });
     });
 
   });
-
 });
 
 http.createServer(bot.middleware()).listen(process.env.PORT);
